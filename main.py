984264db49d8315fcef975581c543e5becc2f888
xim0608 <xim.ryuki@gmail.com>
2018-11-12 12:23:01 +0900

    create base session api

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request, session, redirect, url_for
+import os
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/session', methods=['POST'])
+def new_session():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('top'))
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def delete_session():
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 
 @app.route('/')
 def top():
-    return 'hello! please enter student number'
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    return "hello! username: {}".format(session['username'])
 
 
 if __name__ == '__main__':
