2f7279e6ed7e4c0f40628da797b323dc7667f4a0
xim0608 <xim.ryuki@gmail.com>
2018-11-16 01:59:37 +0900

    add cart 10 limit

--- static/recommend.js
+++ static/recommend.js
@@ -1,29 +1,5 @@
 (function ($) {
-  $('.toggle-cart').on('click', function () {
-    let $el = $(this);
-    let url = '/api/cart/add/' + $el.data('id');
-    $.ajax({
-      type: "GET",
-      url: url,
-      dataType: "json",
-    })
-      .done(function (data) {
-        if (data['status'] === 'added') {
-          $el.text('追加済み');
-          $el.addClass('btn-primary');
-          $el.removeClass('btn-outline-primary');
-          alert('追加されました');
-        } else {
-          $el.text('追加する');
-          $el.addClass('btn-outline-primary');
-          $el.removeClass('btn-primary');
-          alert('削除しました');
-        }
-        check_count()
-      })
-  });
-
-  function check_count() {
+  function changeCount() {
     $.ajax({
       type: "GET",
       url: '/api/cart/count',
@@ -33,5 +9,52 @@
         $('#cart-count').text(data['count']);
       })
   };
+
+
+  $('.toggle-cart').on('click', function () {
+    let $el = $(this);
+
+    let under10 = true;
+
+    $.when(
+      $.ajax({
+        type: "GET",
+        url: '/api/cart/count',
+        dataType: "json",
+      })
+        .done(function (data) {
+          if (data['count'] === 10) {
+            if ($el.text() === "追加する") {
+              alert('すでに10件登録されています');
+              under10 = false;
+            }
+          }
+        })
+    ).done(function () {
+      if (under10) {
+        let url = '/api/cart/add/' + $el.data('id');
+        $.ajax({
+          type: "GET",
+          url: url,
+          dataType: "json",
+        })
+          .done(function (data) {
+            if (data['status'] === 'added') {
+              $el.text('追加済み');
+              $el.addClass('btn-primary');
+              $el.removeClass('btn-outline-primary');
+              alert('追加されました');
+            } else {
+              $el.text('追加する');
+              $el.addClass('btn-outline-primary');
+              $el.removeClass('btn-primary');
+              alert('削除しました');
+            }
+            changeCount()
+          })
+      }
+    })
+  });
+
 
 })(jQuery);
